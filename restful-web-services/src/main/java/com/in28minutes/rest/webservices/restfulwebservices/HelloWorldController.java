9486154e44596c28191a342fce7a3455773ce0b7
furiousdev0622 <furiousdev0622@outlook.com>
2022-12-18 13:56:55 +0000

    content negotiation dependencies and internationalised messages added

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,11 +1,21 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
 
     @GetMapping(path = "/hello-world")
     public String helloWorld(){
@@ -22,5 +32,11 @@ public class HelloWorldController {
         return new HelloWorldBean(
                 String.format("Hello World, %s!", name)
         );
+    }
+
+    @GetMapping(path = "/hello-world-internationalised")
+    public String helloWorldInternationalised(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
     }
 }
